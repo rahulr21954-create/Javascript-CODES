8058305021b9c9f616a80359256546e4ab7c9832
rahulr21954-create <rahulr21954@gmail.com>
2026-06-12 08:46:52 +0000

    datatypes and variables

---
+++ basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=242456;
+let accountEmail="abhi5478@gmail.com";
+var accountState="Verified";
+accountName="Abhinav";
+let accountAddress;
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountState,accountName,accountAddress])
