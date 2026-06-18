7ef95f9d6814ff9c8d69393a0977131c4eca7243
rahulr21954-create <rahulr21954@gmail.com>
2026-06-18 02:47:16 +0000

    Number and Math

---
+++ basics/06_NumberMath.js
@@ -0,0 +1,38 @@
+const score=100;
+console.log(score);
+
+let balance=new Number(543900);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(typeof balance);
+console.log(typeof score);
+
+console.log(balance.toFixed(2));
+
+const anOtherNumber=23.5987;
+console.log(anOtherNumber.toPrecision(2));
+
+const num=10000000;
+console.log(num.toLocaleString());
+
+console.log(num.toLocaleString("en-In"))
+
+//++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.786));
+console.log(Math.ceil(18.27));
+console.log(Math.floor(18.9));
+console.log(Math.min(4,5,8,2));
+console.log(Math.max(7,9,5,3));
+
+console.log((Math.random()*10)+1);
+
+let min=10;
+let max=20;
+ console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+ 
+
